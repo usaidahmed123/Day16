3e733b5b7372cf65354181047304a5ea5c947182
jawad <jawadusaid11@gmail.com>
2022-02-28 22:33:36 +0530

    JDBC

---
+++ JdbcDemo/src/jdbc/JdbcDemo.java
@@ -0,0 +1,33 @@
+package jdbc;
+
+import java.sql.*; // Step 1 import 
+
+public class JdbcDemo {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Class.forName("com.mysql.cj.jdbc.Driver");// step 2  class loader 
+		
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/asd","root", "usaid");//step 3 create connection
+		
+		Statement st = con.createStatement();// step 4
+		//String query = "create table student(ID int primary key,SName varchar(20), SMarks int)";//step 6
+		
+		//String query = "insert into student values(1,'yuv',20)";
+		//String query = "insert into student values(456,'jawad',28)";
+		//String query = "insert into student values(6,'nadeem',2)";
+		String query = "UPDATE student SET SName = 'Aaliyar', SMarks = 555 WHERE ID = 6";
+		 
+		st.executeUpdate(query);//steps 7
+		System.out.println("Student Table created successfully");
+		
+		//System.out.println("Row inserted successfully");
+		
+	st.close();
+	con.close();
+	}
+
+}
+
+
